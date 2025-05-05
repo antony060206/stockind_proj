fe98c7134b120c698ef56b1710e6f8fe343acd5b
Antony <anthony060206@icloud.com>
2025-05-05 13:56:29 -0600

    test

--- stockproj/stockproj/HomePage.xaml.cpp
+++ stockproj/stockproj/HomePage.xaml.cpp
@@ -15,8 +15,6 @@
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace winrt::stockproj::implementation
 {
